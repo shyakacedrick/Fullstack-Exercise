d1d1059341561a1c812eb8c011395dd0eddc2ae1
shyakacedrick <student@sirafrica.org>
2026-03-24 12:04:59 +0300

    Added exercise 1.4 to the file as an update (Array included)

--- part 1/CourseInfo/src/App.jsx
+++ part 1/CourseInfo/src/App.jsx
@@ -1,4 +1,4 @@
-//================================= EXERCISE 1.3 (REFACTORED VERSION) ===================================
+//================================= EXERCISE 1.4 (REFACTORED VERSION) ===================================
 
 
 const Header = (props) => { return <h1>{props.course}</h1> };
@@ -19,9 +19,11 @@ const Total = (props) => <p>Number of exercises: {props.total}</p>;
  const App = () => {
 
   const course = 'Half Stack application development';
-  const part1 = { name : 'Fundamentals of React:', exercises : 10 };
-  const part2 = { name : 'Using props to pass data:', exercises : 7 };
-  const part3 = { name : 'State of a component:', exercises : 14 };
+  const part = [
+    { name : 'Fundamentals of React:', exercises : 10 },
+    { name : 'Using props to pass data:', exercises : 7 },
+    { name : 'State of a component:', exercises : 14 }
+ ];
 
 
   return (    
@@ -29,11 +31,11 @@ const Total = (props) => <p>Number of exercises: {props.total}</p>;
     <div>
       <Header course={course} />
       <Content 
-            part1={part1} 
-            part2={part2}
-            part3={part3}
+            part1={part[0]} 
+            part2={part[1]}
+            part3={part[2]}
         />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Total total={part[0].exercises + part[1].exercises + part[2].exercises} />
 
     </div>
 
