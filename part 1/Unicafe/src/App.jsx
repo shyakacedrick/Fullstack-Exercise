3a6dc221ada30faa7e56dc218d2f5975a331c03c
shyakacedrick <student@sirafrica.org>
2026-03-24 16:21:52 +0300

    Update the code to exercise 1.8 (step 3)

--- part 1/Unicafe/src/App.jsx
+++ part 1/Unicafe/src/App.jsx
@@ -1,29 +1,36 @@
-//=========================== EXERCISE 1.6 UNICAFE ===============
+//=========================== EXERCISE 1.7 UNICAFE ===============
 
 import { useState } from "react";
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+    </div>
+  );
+};
+
+
+
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const all = good + neutral + bad;
-  const average = (good - bad) / all || 0;
-  const positive = (good / all) * 100 || 0;
 
   return (
     <div>
       <h1>Give feedback</h1>
+
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h2>Feedback so far</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>average: {average.toFixed(1)}</p>
-      <p>positive: {positive.toFixed(1)}%</p>
+      <h2>Statistics</h2>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
