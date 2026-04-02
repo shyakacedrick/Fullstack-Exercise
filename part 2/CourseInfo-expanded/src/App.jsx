afe4541a0b2824cdf9632bc76657628abba59b4d
shyakacedrick <student@sirafrica.org>
2026-04-02 11:18:44 +0300

    Separated modules as required in Exercise 2.1

--- part 2/CourseInfo-expanded/src/App.jsx
+++ part 2/CourseInfo-expanded/src/App.jsx
@@ -1,4 +1,4 @@
-//================================= EXERCISE 2.1 (COURSE INFORMATION EXPANDED) ===================================
+//================================= EXERCISE 2.1 (COURSE INFORMATION EXPANDED (step 6)) ===================================
 
 import React from 'react'
 import Course from './Course';
@@ -6,17 +6,17 @@ import Course from './Course';
 
  const App = () => {
   const course = {
+    id: 1,
     name : 'Half Stack application development',
     parts : [
-      { name : 'Fundamentals of React:', exercises : 10 }, 
-      { name : 'Using props to pass data:', exercises : 7 },
-      { name : 'State of a component:', exercises : 14 }
+      { id: 1, name : 'Fundamentals of React:', exercises : 10 }, 
+      { id: 2, name : 'Using props to pass data:', exercises : 7 },
+      { id: 3, name : 'State of a component:', exercises : 14 }
     ]   
   }
   console.log(course)
 
   return (    
-
     <div>
       <Course course={course}/>
     </div>
--- part 2/CourseInfo-expanded/src/Course.jsx
+++ part 2/CourseInfo-expanded/src/Course.jsx
@@ -1,32 +1,27 @@
 import React from 'react'
 
 const Header = ({course}) => { return <h1>{course}</h1> };
-const Part = ({name, exercises}) => { return <p>{name} {exercises}</p> };
+const Part = ({part}) => { return <p>{part.name} {part.exercises}</p> };
+
 const Content = ({parts}) => {
   return (
       <div>
-        <Part name={parts[0].name} exercises={parts[0].exercises} />
-        <Part name={parts[1].name} exercises={parts[1].exercises} />
-        <Part name={parts[2].name} exercises={parts[2].exercises} />
+        {parts.map(part => <Part key={part.id} part={part} />)}
       </div>
     )
   };
-const Total = (props) => <p> Number of exercises: {props.total}</p>;
+const Total = ({total}) => <p> Number of exercises: {total}</p>;
 
 
 const Course = ({course}) => {
-    
+  
   return (
     <div>
       <Header course = {course.name} />
       <Content parts = {course.parts}/>
-      <Total total = 
-      {course.parts[0].exercises + 
-       course.parts[1].exercises + 
-       course.parts[2].exercises} />
+      <Total total = {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </div>
   )
 }
 
 export default Course
-
