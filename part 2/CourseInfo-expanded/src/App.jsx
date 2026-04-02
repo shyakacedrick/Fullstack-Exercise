709700a7a52b0ab256e997abde85b18cbd5d4def
shyakacedrick <student@sirafrica.org>
2026-04-02 10:41:27 +0300

    Fix: removed submodule, added part 2 correctly

---
+++ part 2/CourseInfo-expanded/src/App.jsx
@@ -0,0 +1,27 @@
+//================================= EXERCISE 2.1 (COURSE INFORMATION EXPANDED) ===================================
+
+import React from 'react'
+import Course from './Course';
+
+
+ const App = () => {
+  const course = {
+    name : 'Half Stack application development',
+    parts : [
+      { name : 'Fundamentals of React:', exercises : 10 }, 
+      { name : 'Using props to pass data:', exercises : 7 },
+      { name : 'State of a component:', exercises : 14 }
+    ]   
+  }
+  console.log(course)
+
+  return (    
+
+    <div>
+      <Course course={course}/>
+    </div>
+  )
+}
+
+export default App
+
