8b7b5af221aa6afef013d7fad55952398d1c9663
shyakacedrick <student@sirafrica.org>
2026-04-09 15:24:47 +0300

    Update the entire to a refactored version as it complete Exercise 2.10 of PhoneBook

--- part 2/PhoneBook/src/App.jsx
+++ part 2/PhoneBook/src/App.jsx
@@ -1,63 +1,78 @@
 import React, {useState} from 'react'
+import './index.css'
+import PersonList from './components/PersonList'
+import Form from './components/form'
 
+const Filter = ({value, onChange}) => (
+  <input 
+    className='val5'
+    value={value} 
+    onChange={onChange} 
+    placeholder="Search name" 
+  />
+)
 const App = () => {
-  const [notes, setNote] = useState([
-    {id: 1, name: 'Ronaldinho', content: 'I learned all about life with a ball at my feet.', yearOfBirth: 1980, isSingle: true},
-    {id: 2, name: 'Messi', content: 'I\'m more worried about being a good person than being the best football player in the world.', yearOfBirth: 1985, isSingle: false},
-    {id: 3, name: 'Christiano', content: 'Talent without working hard is nothing.', yearOfBirth: 1987, isSingle: false},
-    {id: 4, name: 'Mbappe', content: 'It\'s not over until I win', yearOfBirth: 1998, isSingle: true},
+  const [persons, setPersons] = useState([
+    {id: 1, name: 'Christiano', quote: 'Talent without working hard is nothing.', yearOfBirth: 1987, isSingle: false},
+    {id: 2, name: 'Ronaldinho', quote: 'I learned all about life with a ball at my feet.', yearOfBirth: 1980, isSingle: true},
+    {id: 3, name: 'Messi', quote: 'I\'m more worried about being a good person than being the best football player in the world.', yearOfBirth: 1985, isSingle: true},
+    {id: 4, name: 'Mbappe', quote: 'It\'s not over until I win', yearOfBirth: 1998, isSingle: true},
   ])
   
   const [newNote, setNewNote] = useState('')
-  const [newContent, setNewContent] = useState('')
-  const [yearOfBirth, setNumber] = useState('')
+  const [yearOfBirth, setYearOfBirth] = useState('')
+  const [newQuote, setNewQuote] = useState('')
   const [filter, setFilter] = useState ('')
 
   const AddName = (e) => {
     e.preventDefault()
     
-    const duplicate = notes.find(n => n.name.toLowerCase() === newNote.toLowerCase())
+    const duplicate = persons.find(n => n.name.toLowerCase() === newNote.toLowerCase())
     if (duplicate) { alert( `${newNote} already exist in the PhoneBook`); return }
 
     const addedNote = {
       id: String(Date.now()),
       name: newNote,
-      content: newContent,
+      quote: newQuote,
       yearOfBirth: yearOfBirth,
       isSingle: Math.random() > 0.5,
     }
 
-    setNote([...notes, addedNote])
+    setPersons([...persons, addedNote])
     setNewNote('')
-    setNewContent('')
-    setNumber('')
+    setNewQuote('')
+    setYearOfBirth('')
 
     console.table(addedNote)
   }
+
+  const noteHandler = (e) => setNewNote(e.target.value)
+  const quoteHandler = (e) =>  setNewQuote(e.target.value)
+  const yearHandler = (e) =>  setYearOfBirth(e.target.value)
    
   const handleSearch = (e)=>{ setFilter(e.target.value); console.log(e.target.value)}
-  const search = notes.filter(note => note.name.toLowerCase().includes(filter.toLowerCase()))
-
-  //console.log('Name: ', newNote , "\n", 'Year: ', yearOfBirth)
+  const filteredPersons = persons.filter(note => note.name.toLowerCase().includes(filter.toLowerCase()))
 
   return (
-    <div>
+    <div className='parent'>
 
-      <div>
-        <input value={filter} onChange={handleSearch} placeholder="Search name"/>
+      <div className='filter'>
+        <Filter value={filter} onChange={handleSearch}/>
       </div>
 
-      <h1>PhoneBook</h1>
-        <form onSubmit={AddName}>
-          <input value={newNote} onChange={(e) => setNewNote(e.target.value)}  placeholder='Add Name'/> &nbsp;
-          <input value={yearOfBirth} onChange={(e) => setNumber(e.target.value)} placeholder='Add year Of Birth'/> <pre />
-          <button type='submit'>Add</button>
-        </form>
+      <div className='form'>
+        <h1 id='title'>PhoneBook</h1>
+        <Form 
+          onSubmit={AddName} 
+          noteValue={newNote} quoteValue={newQuote} yearValue={yearOfBirth} 
+          noteHandler={noteHandler} quoteHandler={quoteHandler} yearHandler={yearHandler}
+        />
+      </div>
       
-      <h2>Names</h2>
-        <ul>
-          {search.map(n => <li key={n.id}>{n.name} : {n.yearOfBirth}</li>)}
-        </ul>
+      <div className='names'>
+        <h2>Names</h2>
+        <PersonList persons={filteredPersons}/>
+      </div>
     </div>
   )
 }
