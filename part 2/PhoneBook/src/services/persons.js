31f1961f2230fd68732787c2ee29daa8b61dedb2
shyakacedrick <student@sirafrica.org>
2026-04-14 12:39:43 +0300

    created a services directory to store parsons.js file for easy and meaningfull importation'

---
+++ part 2/PhoneBook/src/services/persons.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => axios.get(baseUrl).then(res => res.data)
+const create = (newObject) => axios.post(baseUrl, newObject).then(res => res.data)
+const remove = (id) => axios.delete(`${baseUrl}/${id}`)
+const update = (id, newObject) => axios.put(`${baseUrl}/${id}`, newObject).then(res => res.data) 
+
+
+export default { getAll, create, remove, update }
