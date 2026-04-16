81844c9c60713d1ebb961fedd243c4ab2ffb7480
shyakacedrick <student@sirafrica.org>
2026-04-16 16:37:52 +0300

    Exercise 2.20: Completed adding the weather API. This is the final of Part 2

--- part 2/Data For Countries/src/App.jsx
+++ part 2/Data For Countries/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import countryService from './services/countries'
+import Weather from './components/Weather'
 
 const App = () => {
   const [query, setQuery] = useState('')
@@ -14,34 +15,70 @@ const App = () => {
   )
 
   return (
-    <div>
-      find countries <input value={query} onChange={(e) => setQuery(e.target.value)} />
-      
-      {matches.length > 10 && <p>Too many matches, specify another filter</p>}
-      
+    <div className='container'>
+
+    <div className='searchBar'>
+      <div className='title    '>
+        <h1><i>Get information of any counry of your liking</i></h1>
+      </div>
+      <div className='search-holder'>
+        <input 
+          className='searchInput'
+          value={query} 
+          onChange={(e) => setQuery(e.target.value)} 
+          placeholder='Find Countries'
+        />
+
+        {matches.length > 10 && (
+          <p id='filterPara'>Too many matches, specify another filter</p>
+        )}
+      </div>
+
+      <div className='suggestionContainer'>
         {matches.length <= 10 && matches.length > 1 && (
-          matches.map(c => (
-            <div key={c.cca3}>
-              {c.name.common} 
-              <button onClick={() => setQuery(c.name.common)}>show</button>
-            </div>
+        matches.map(c => (
+          <div key={c.cca3} className='suggestionDisplay'>
+            <p className='sug-para'>{c.name.common}</p>
+            <button onClick={() => setQuery(c.name.common)} className='btn'>
+              show
+            </button>
+          </div>
           ))
         )}
+      </div>
+    </div>
 
       {matches.length === 1 && (
-        <div>
-          <h1>{matches[0].name.common}</h1>
-          <p>capital {matches[0].capital[0]}</p>
-          <p>area {matches[0].area}</p>
-          <b>languages:</b>
-          <ul>
-            {Object.values(matches[0].languages).map(l => <li key={l}>{l}</li>)}
-          </ul>
-          <img src={matches[0].flags.png} width="150" />
+        <div className='card'>
+
+          <div className='flag'>
+            <img 
+              src={matches[0].flags.png} 
+              // width="150" 
+              alt="flag"
+            />
+          </div>
+
+          <div className="country-details">
+            <div className='Details'>
+              <h1>{matches[0].name.common}</h1>
+              <p>capital {matches[0].capital[0]}</p>
+              <p>area {matches[0].area}</p>
+
+              <b>languages:</b>
+              <ul>
+                {Object.values(matches[0].languages).map(l => (
+                  <li key={l}>{l}</li>
+                ))}
+              </ul>
+            </div>
+          
+            <div className="weather">
+              <Weather city={matches[0].capital[0]} />
+            </div>
+          </div>
         </div>
       )}
-
-
     </div>
   )
 }
