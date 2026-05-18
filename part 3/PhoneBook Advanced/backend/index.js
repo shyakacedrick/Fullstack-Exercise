1847a4bd41ca35a45106841b877b5e04913cd9a8
shyakacedrick <student@sirafrica.org>
2026-05-18 16:49:05 +0300

    Exercise 3.17: Now the frontend supports updating from backend

--- part 3/PhoneBook Advanced/backend/index.js
+++ part 3/PhoneBook Advanced/backend/index.js
@@ -31,6 +31,15 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) res.json(person)
+      else res.status(404).end()
+    })
+    .catch(error => next(error))
+})
+
 app.post('/api/persons', (req, res, next) => {
   const { name, number } = normalizePerson(req.body)
 
@@ -57,6 +66,27 @@ app.delete('/api/persons/:id', (req, res, next) => {
 })
 
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = normalizePerson(req.body)
+
+  const updatedPerson = {
+    name,
+    number
+  }
+
+  Person.findByIdAndUpdate(
+    req.params.id,
+    updatedPerson,
+    { new: true }
+  )
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+
 const errorHandler = (error, req, res, next) => {
   console.error(error.message)
 
@@ -64,7 +94,6 @@ const errorHandler = (error, req, res, next) => {
 
   next(error)
 }
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
