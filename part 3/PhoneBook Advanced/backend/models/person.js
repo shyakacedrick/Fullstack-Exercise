8882990e9335701d377e1e27b40bc021df8e3fc1
shyakacedrick <student@sirafrica.org>
2026-05-21 15:45:45 +0300

    Exercise 3.20: Added a custom validation to the application, which will make sure that phone numbers are of the correct form

--- part 3/PhoneBook Advanced/backend/models/person.js
+++ part 3/PhoneBook Advanced/backend/models/person.js
@@ -22,7 +22,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v) && v.length >= 8;
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  },
 })
 
 
