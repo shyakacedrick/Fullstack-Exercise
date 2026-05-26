36d195c561a1026a9fa613f85733682392106bc2
shyakacedrick <student@sirafrica.org>
2026-05-26 13:59:59 +0300

    Exercise 4.4: Defined a new totalLikes function that receives a list of blog posts as a parameter. The function returns the total sum of likes in all of the blog posts.

--- part 4/Blog List/test/list_helper.test.js
+++ part 4/Blog List/test/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 
 const listHelper = require('../utils/list_helper')
@@ -9,4 +9,37 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
 
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('of bigger list is calculated right', () => {
+    const blogs = [
+      { likes: 5 },
+      { likes: 10 },
+      { likes: 7 }
+    ]
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 22)
+  })
 })
