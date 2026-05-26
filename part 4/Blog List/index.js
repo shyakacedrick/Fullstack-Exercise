83d205f4dec1f99d28a4e3ecec56b7865cc58daa
shyakacedrick <student@sirafrica.org>
2026-05-26 11:01:13 +0300

    Exercise 4.2:Refactored the application into separate modules

--- part 4/Blog List/index.js
+++ part 4/Blog List/index.js
@@ -1,41 +1,58 @@
-require('dotenv').config()
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const express = require('express')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
 
-const app = express()
 
-app.use(express.json())
 
-mongoose.connect(process.env.MONGODB_URI, { family: 4 })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+
+
+
+
+
+
+
+// require('dotenv').config()
+
+// const express = require('express')
+// const mongoose = require('mongoose')
+// const Blog = require('./models/blog')
+
+// const app = express()
+
+// app.use(express.json())
+
+// mongoose.connect(process.env.MONGODB_URI, { family: 4 })
+//   .then(() => {
+//     console.log('connected to MongoDB')
+//   })
+//   .catch(error => {
+//     console.log('error connecting to MongoDB:', error.message)
+//   })
 
   
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog.find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
 
-  blog.save()
-    .then(savedBlog => {
-      response.status(201).json(savedBlog)
-    })
-})
+//   blog.save()
+//     .then(savedBlog => {
+//       response.status(201).json(savedBlog)
+//     })
+// })
 
-const PORT = process.env.PORT
+// const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
