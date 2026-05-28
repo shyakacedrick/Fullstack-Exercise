214388ad49eec5cbfebad091639980538987e4c2
shyakacedrick <student@sirafrica.org>
2026-05-28 13:27:03 +0300

    Exercise 4.15: Implemented a way to create new users by doing an HTTP POST request to address api/users. Users have a username, password and name.

--- part 4/Blog List Expansion/app.js
+++ part 4/Blog List Expansion/app.js
@@ -5,21 +5,24 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 
 const app = express()
 
 mongoose.connect(config.MONGODB_URI, { family: 4 })
-  .then(() => {
+.then(() => {
     logger.info('connected to MongoDB')
   })
   .catch(error => {
     logger.error('error connecting to MongoDB:', error.message)
   })
-
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogsRouter)
+  
+  app.use(express.json())
+  app.use(middleware.requestLogger)
+  
+  app.use('/api/blogs', blogsRouter)
+  app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ part 4/Blog List Expansion/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ]
+})
+
+module.exports = mongoose.model('User', userSchema)
