5d3158ffe5d02214db2a1d0e2743c7b7538fac76
shyakacedrick <student@sirafrica.org>
2026-05-28 12:31:06 +0300

    Exercise 4.13: Implemented functionality for deleting a single blog post resource.

---
+++ part 4/Blog List Expansion/test/blog_api.test.js
@@ -0,0 +1,136 @@
+const { test, describe, beforeEach, after } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('when there are initially some blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(
+      response.body.length,
+      helper.initialBlogs.length
+    )
+  })
+})
+
+test('blog posts have id property', async () => {
+  const response = await api.get('/api/blogs')
+
+  const blog = response.body[0]
+
+  assert(blog.id)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Async/Await Guide',
+    author: 'Cedric',
+    url: 'https://example.com',
+    likes: 15
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(
+    response.body.length,
+    helper.initialBlogs.length + 1
+  )
+
+  const titles = response.body.map(blog => blog.title)
+
+  assert(titles.includes('Async/Await Guide'))
+})
+
+test('if likes property missing, it defaults to 0', async () => {
+  const newBlog = {
+    title: 'No Likes Blog',
+    author: 'Anonymous',
+    url: 'https://example.com'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  assert.strictEqual(response.body.likes, 0)
+})
+
+test('blog without title is not added', async () => {
+  const newBlog = {
+    author: 'Someone',
+    url: 'https://example.com',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+test('blog without url is not added', async () => {
+  const newBlog = {
+    title: 'Missing URL',
+    author: 'Someone',
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+})
+
+describe('deletion of a blog', () => {
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await Blog.find({})
+
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await Blog.find({})
+
+    assert.strictEqual(
+      blogsAtEnd.length,
+      helper.initialBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+
+    assert(!titles.includes(blogToDelete.title))
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
---
+++ part 4/Blog List Expansion/test/test_helper.js
@@ -0,0 +1,18 @@
+const initialBlogs = [
+  {
+    title: 'First blog',
+    author: 'Ceddy',
+    url: 'https://example.com/1',
+    likes: 5
+  },
+  {
+    title: 'Second blog',
+    author: 'John',
+    url: 'https://example.com/2',
+    likes: 10
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
