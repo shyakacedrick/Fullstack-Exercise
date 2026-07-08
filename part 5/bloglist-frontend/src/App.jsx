07f55f522047dd8e8ec430db006db98a16cfdc85
shyakacedrick <student@sirafrica.org>
2026-07-08 11:47:00 +0300

    Exercise 5.2: Persist login with localStorage and add logout

--- part 5/bloglist-frontend/src/App.jsx
+++ part 5/bloglist-frontend/src/App.jsx
@@ -22,11 +22,28 @@ const App = () => {
     })
   }, [])
 
+  useEffect(() => {
+  const loggedUserJSON = window.localStorage.getItem(
+    'loggedBlogAppUser'
+  )
+
+  if (loggedUserJSON) {
+    const user = JSON.parse(loggedUserJSON)
+    setUser(user)
+  }
+}, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
     try {
       const user = await loginService.login({ username, password })
+
+      // Save to browser
+      window.localStorage.setItem(
+        'loggedBlogAppUser',
+        JSON.stringify(user)
+      )
 
       setUser(user)
       setUsername('')
@@ -54,12 +71,21 @@ const App = () => {
     )
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
+  }
+
   return (
     <main className="blog-list">
       <h2>Blogs</h2>
 
-      <p>{user.name} logged in</p>
-
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>
+          Logout
+        </button>
+      </p>
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
       ))}
