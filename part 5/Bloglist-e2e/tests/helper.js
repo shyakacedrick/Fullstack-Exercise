38745e27165e81ad3de92ca0ded6fb66d0572631
shyakacedrick <student@sirafrica.org>
2026-07-21 17:20:37 +0300

    Exercise 5.27: Modified the tests for the single blog view created in Vitest

--- part 5/Bloglist-e2e/tests/helper.js
+++ part 5/Bloglist-e2e/tests/helper.js
@@ -1,33 +1,23 @@
-const { expect } = require('@playwright/test')
-
 const loginWith = async (page, username, password) => {
-  await page.getByLabel('Username').fill(username)
-  await page.getByLabel('Password').fill(password)
 
-  await page.getByRole('button', {
-    name: 'Login'
-  }).click()
+  const response = await page.request.post(
+    'http://localhost:3003/api/login',
+    {
+      data: {
+        username,
+        password,
+      },
+    }
+  )
 
-  await expect(
-    page.getByRole('button', { name: 'Logout' })
-  ).toBeVisible()
-}
+  const user = await response.json()
 
-const createBlog = async (page, blog) => {
-  await page.getByRole('button', {
-    name: 'New Blog'
-  }).click()
+  await page.evaluate(user => {
+    localStorage.setItem(
+      'loggedBlogAppUser',
+      JSON.stringify(user)
+    )
+  }, user)
 
-  await page.getByLabel('Title').fill(blog.title)
-  await page.getByLabel('Author').fill(blog.author)
-  await page.getByLabel('URL').fill(blog.url)
-
-  await page.getByRole('button', {
-    name: 'Create'
-  }).click()
-}
-
-module.exports = {
-  loginWith,
-  createBlog
+  await page.goto('http://localhost:5173')
 }
