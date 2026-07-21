db1ab05e15231328b03af521d31b050cfb320a15
shyakacedrick <student@sirafrica.org>
2026-07-21 14:26:55 +0300

    Exercise 5.18: Tested both successful and failed login.

--- part 5/Bloglist-e2e/tests/blog-app.spec.js
+++ part 5/Bloglist-e2e/tests/blog-app.spec.js
@@ -1,11 +1,13 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
+
   beforeEach(async ({ page }) => {
     await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
+
     await expect(
       page.getByRole('heading', { name: /log in/i })
     ).toBeVisible()
@@ -19,7 +21,22 @@ describe('Blog app', () => {
     ).toBeVisible()
 
     await expect(
-      page.getByRole('button', { name: /login/i })
+      page.getByRole('button', { name: 'Login' })
+    ).toBeVisible()
+
+  })
+
+  test('user can log in', async ({ page }) => {
+
+    await page.getByLabel('Username').fill('innocent')
+
+    await page.getByLabel('Password').fill('innocent1234')
+
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(
+      page.getByRole('button', { name: 'Logout' })
     ).toBeVisible()
   })
+
 })
