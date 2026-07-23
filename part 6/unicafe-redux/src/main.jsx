eb35ce5e557c45dfe0cbd4527808aff4b6beea9d
shyakacedrick <student@sirafrica.org>
2026-07-23 16:36:50 +0300

    Exercise 6.1: handled the application's state management using Zustand.

--- part 6/unicafe-redux/src/main.jsx
+++ part 6/unicafe-redux/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import store from './reducers/counterReducer'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+
+store.subscribe(renderApp)
---
+++ part 6/unicafe-redux/src/reducers/counterReducer.js
@@ -0,0 +1,39 @@
+import { createStore } from 'redux'
+
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0,
+}
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'GOOD':
+      return {
+        ...state,
+        good: state.good + 1,
+      }
+
+    case 'OK':
+      return {
+        ...state,
+        ok: state.ok + 1,
+      }
+
+    case 'BAD':
+      return {
+        ...state,
+        bad: state.bad + 1,
+      }
+
+    case 'ZERO':
+      return initialState
+
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+export default store
