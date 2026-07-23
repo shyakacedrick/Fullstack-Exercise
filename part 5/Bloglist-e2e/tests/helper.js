36c204dde186439e73899a13064c988cf4cfa1d5
shyakacedrick <student@sirafrica.org>
2026-07-23 09:35:20 +0300

    Exercise 5.28: fixed the end-to-end tests created with Playwright.

--- part 5/Bloglist-e2e/tests/helper.js
+++ part 5/Bloglist-e2e/tests/helper.js
@@ -21,3 +21,25 @@ const loginWith = async (page, username, password) => {
 
   await page.goto('http://localhost:5173')
 }
+
+
+const createBlog = async (page, blog) => {
+
+  const loggedUserJSON = await page.evaluate(() =>
+    localStorage.getItem('loggedBlogAppUser')
+  )
+
+  const user = JSON.parse(loggedUserJSON)
+
+  await page.request.post(
+    'http://localhost:3003/api/blogs',
+    {
+      data: blog,
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    }
+  )
+
+  await page.goto('http://localhost:5173')
+}
