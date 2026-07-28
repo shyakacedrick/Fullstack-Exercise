a7b89f3be67f7b4c296753778755da53ce33f1df
shyakacedrick <student@sirafrica.org>
2026-07-28 12:54:24 +0300

    Exercise 6.17: Added new anecdotes with TanStack Query

--- part 6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part 6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,19 +1,39 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['anecdotes'],
+      })
+    },
+  })
+
   const onCreate = (event) => {
     event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.reset()
-    console.log('new anecdote')
+
+    const content = event.target.anecdote.value.trim()
+
+    if (!content) return
+
+    newAnecdoteMutation.mutate(content)
+
+    event.target.anecdote.value = ''
   }
 
   return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name="anecdote" />
-        <button type="submit">create</button>
-      </form>
-    </div>
+    <form onSubmit={onCreate}>
+      <input name="anecdote" />
+
+      <button type="submit">
+        create
+      </button>
+    </form>
   )
 }
 
--- part 6/query-anecdotes/src/requests.js
+++ part 6/query-anecdotes/src/requests.js
@@ -4,8 +4,37 @@ export const getAnecdotes = async () => {
   const response = await fetch(baseUrl)
 
   if (!response.ok) {
-    throw new Error('anecdote service not available due to problems in server')
+    throw new Error(
+      'anecdote service not available due to problems in server'
+    )
   }
+
+  return await response.json()
+}
+
+export const createAnecdote = async (content) => {
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content,
+      votes: 0,
+    }),
+  })
+
+  return await response.json()
+}
+
+export const updateAnecdote = async (anecdote) => {
+  const response = await fetch(`${baseUrl}/${anecdote.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(anecdote),
+  })
 
   return await response.json()
 }
