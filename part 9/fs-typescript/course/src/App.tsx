f0bb16ff5b9a58b3289c41f289c5a3db1377a955
shyakacedrick <student@sirafrica.org>
2026-09-10 12:59:54 +0300

    Exercise 9.17: Course step1 - Header, Content and Total components with TypeScript

---
+++ part 9/fs-typescript/course/src/App.tsx
@@ -0,0 +1,70 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+interface HeaderProps {
+  name: string;
+}
+
+interface ContentProps {
+  courseParts: CoursePart[];
+}
+
+interface TotalProps {
+  totalExercises: number;
+}
+
+const Header = (props: HeaderProps) => {
+  return <h1>{props.name}</h1>;
+};
+
+const Content = (props: ContentProps) => {
+  return (
+    <div>
+      {props.courseParts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const Total = (props: TotalProps) => {
+  return (
+    <p>
+      Number of exercises {props.totalExercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts: CoursePart[] = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total totalExercises={totalExercises} />
+    </div>
+  );
+};
+
+export default App;
